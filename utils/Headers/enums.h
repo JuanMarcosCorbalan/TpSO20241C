2100f23645ec9b9c8552319ac86fd747bd7526a9
ReoP159 <rodri_olea@hotmail.com>
2024-06-27 19:42:42 +0000

    Se agrgan las funciones para serializar y descelrializar mensajes entre Memoria y FS
    Co-authored-by: CamilaAlbite <cami.albite@gmail.com>

--- utils/Headers/enums.h
+++ utils/Headers/enums.h
@@ -29,7 +29,9 @@ typedef enum cod_op {
 	MSG_IO_STDIN_READ,
 	MSG_MOV_OUT,
 	MSG_MOV_IN,
-	MSG_COPY_STRING
+	MSG_COPY_STRING,
+	MSG_IO_FS_READ,
+	MSG_IO_FS_WRITE
 } cod_op;
 
 typedef enum tipo_interfaz {
